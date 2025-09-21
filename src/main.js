fceb94f80c07bd468e8204d6a97dce8289016a0a
1PISYN228 <kastromatvuy@gmail.com>
2025-09-21 20:34:57 +0300

    build the app

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import initBurgerMenu from "@/modules/burgerMenu"
-import validateForm from "./modules/validateForm"
-import openAnswer from "./modules/questionAnswer"
-import scrollButtons from "./modules/scrollButtons"
-
-initBurgerMenu()
-validateForm()
-openAnswer()
-scrollButtons()
