1351060bb23c69d5117984533851683b5530229f
1PISYN23 <kastromatvuy@gmail.com>
2025-09-21 20:37:29 +0300

    Update scrollButtons.js

--- src/modules/scrollButtons.js
+++ src/modules/scrollButtons.js
@@ -14,17 +14,17 @@ const scrollButtons = () => {
     
     const infoTab = [
         {   
-            image: "/images/illustration-features-tab-1.svg", 
+            image: "/public/images/illustration-features-tab-1.svg", 
             title: "Bookmark in one click", 
             text: "Organize your bookmarks however you like. Oursimple drag-and-drop interface gives you completecontrol over how you manage your favourite sites."
         },
         { 
-            image: "/images/illustration-features-tab-2.svg", 
+            image: "/public/images/illustration-features-tab-2.svg", 
             title: "Intelligent search", 
             text: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
         },
         { 
-            image: "/images/illustration-features-tab-3.svg", 
+            image: "/public/images/illustration-features-tab-3.svg", 
             title: "Share your bookmarks", 
             text: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
         },
